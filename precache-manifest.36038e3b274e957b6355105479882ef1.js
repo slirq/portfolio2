40461f73a7da390bdcec2756d16fdf1d9a8790f0
abdullah <abdullahshaikh136@gmail.com>
2020-03-09 22:00:16 +0530

    Updates

---
+++ precache-manifest.36038e3b274e957b6355105479882ef1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed4b133d64df3974951e11f4dee074b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5d623e88c023253a4010",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "f8a0f0aed01b05b53be1",
+    "url": "/portfolio/static/css/main.1872ccaf.chunk.css"
+  },
+  {
+    "revision": "5d623e88c023253a4010",
+    "url": "/portfolio/static/js/2.e60d0c56.chunk.js"
+  },
+  {
+    "revision": "a1e12f97867665430e5c8e39fc9f180a",
+    "url": "/portfolio/static/js/2.e60d0c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a0f0aed01b05b53be1",
+    "url": "/portfolio/static/js/main.b35eb8e8.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "ab88ab80686a927b3016225fcfc72e4b",
+    "url": "/portfolio/static/media/mypic.ab88ab80.jpg"
+  }
+]);
